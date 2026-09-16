ced9d094497793a5c994ba7e10603aaeb7b47ce6
afnan <muhammadafnan88292@gmail.com>
2026-09-16 14:23:56 +0500

    add all set up for project 06

---
+++ project-06/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+ function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default Home;
